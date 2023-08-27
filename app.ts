abda9c500f2cb5c947f23449370971c051dae3a5
Bruno Wolff <bruno.wolff@clinicorp.com>
2023-08-26 21:13:12 -0300

    feat(app.ts): add functionality to save telemetry data to the database
    - Import `PrismaClient` from `@prisma/client` to establish a connection to the database
    - Create an instance of `PrismaClient`
    - Modify the `main` function to accept an array of `LabelTelemetry` objects as input
    - Extract `deviceCode` and `passengerCount` from the first element of the `data` array
    - Use `PrismaClient` to find the device with the matching `deviceCode`
    - Throw an error if the device is not found
    - Use `PrismaClient` to create a new telemetry entry in the database with the device ID and passenger count
    
    chore(package.json): remove unused dependencies
    
    - Remove `@aws-sdk/client-dynamodb`, `@aws-sdk/client-rekognition`, `@aws-sdk/client-sns`, and `@types/aws-lambda` dependencies from `package.json`
    - Keep only the necessary dependencies: `@prisma/client` and `nanoid`

--- app.ts
+++ app.ts
@@ -1,5 +1,30 @@
-const main = async (data: any) => {
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async (data: LabelTelemetry[]) => {
   console.info("Chegou no put", data);
+  const { deviceCode, passengerCount } = data[0];
+
+  const device = await prisma.device.findFirst({
+    where: {
+      code: data[0].deviceCode,
+    },
+  });
+
+  if (!device) throw new Error("Device not found");
+
+  return prisma.telemetry.create({
+    data: {
+      deviceId: device.id,
+      passengerCount: passengerCount,
+    }
+  })
 };
+
+type LabelTelemetry = {
+  deviceCode: string;
+  passengerCount: number;
+}
 
 export { main };
